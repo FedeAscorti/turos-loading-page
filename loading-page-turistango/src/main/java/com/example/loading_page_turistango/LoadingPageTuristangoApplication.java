8e1c1292352c9f5cb1dab16ce1dd9309f49c962b
FedeAscorti <103160219+FedeAscorti@users.noreply.github.com>
2024-10-22 17:19:51 -0300

    Add files via upload

---
+++ loading-page-turistango/src/main/java/com/example/loading_page_turistango/LoadingPageTuristangoApplication.java
@@ -0,0 +1,13 @@
+package com.example.loading_page_turistango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadingPageTuristangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadingPageTuristangoApplication.class, args);
+	}
+
+}
---
+++ loading-page-turistango/src/test/java/com/example/loading_page_turistango/LoadingPageTuristangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loading_page_turistango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadingPageTuristangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
